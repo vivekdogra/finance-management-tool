9ea43e59eb71f2d631d9a16dfa5b5634b277a2a7
Deendayal Garg <Deendayal.Garg@paypal.com>
2014-11-08 21:04:54 +0530

    bill submit module

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@
 
 
 var IndexModel = require('../models/index');
+var fs = require('fs');
 
 
 module.exports = function (router) {
@@ -11,8 +12,19 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
         
-        res.render('index', model);
+        res.render('submitbill', model);
         
     });
+
+    router.post('/submitbills', function (req, res) {
+
+        fs.readFile(req.files.billfile.path, function (err, data) {
+            var newPath = __dirname+ "/../uploads/" + req.param('BillNumber')+".jpg" ;
+            fs.writeFile(newPath, data, function (err) {
+                res.redirect("/");
+            });
+        });
+    });
+
 
 };
