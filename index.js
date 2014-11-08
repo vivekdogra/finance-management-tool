5b9b770fc647d4833c4c3aaf917aa533f0f11b2f
vivekdogra <vivek.dogra.iitd@gmail.com>
2014-11-08 21:51:57 +0530

    Taking the config code out of main index.js and putting it in spec.js which does all the database connectivity and other set up.

--- index.js
+++ index.js
@@ -3,18 +3,14 @@
 
 var kraken = require('kraken-js'),
     app = require('express')(),
-    options = {
-        onconfig: function (config, next) {
-            //any config setup/overrides here
-            next(null, config);
-        }
-    },
-    port = process.env.PORT || 8001;
+    options = require('./lib/spec')(app),
+//    userLib = require('./lib/user')(),
+    port = process.env.PORT || 8005;
+
 
 
 app.use(kraken(options));
 
-
-app.listen(port, function (err) {
+app.listen(port, function(err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
 });
